dcb10b16d7b3926c23eb9939cbf9dd00be1444d2
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-09-01 21:16:09 +0600

    added category-btn

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
-console.log('scripts')
+const categoryBtnContainer = document.getElementById('category-btn-container');
+categoryBtnContainer.classList = 'mt-8 flex justify-center gap-6';
+categoryBtnContainer.innerHTML = `
+    <div>
+        <button class="btn bg-gray-300 px-4 py-2 font-medium">All</button>
+        </div>
+        <div>
+        <button class="btn bg-gray-300 px-4 py-2 font-medium">Music</button>
+        </div>
+        <div>
+        <button class="btn bg-gray-300 px-4 py-2 font-medium">Comedy</button>
+        </div>
+        <div>
+        <button class="btn bg-gray-300 px-4 py-2 font-medium">Drawing</button>
+    </div>
+`
